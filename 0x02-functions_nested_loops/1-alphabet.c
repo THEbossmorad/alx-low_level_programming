aa5409e766a66172a6b8886e4bceb655294bd2bb
THEbossmorad <morad.abbous@gmail.com>
2023-02-21 16:19:13 +0100

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,9 @@
 
 /**
  *  * print_alphabet - utilizes on the _putchar function to print
- *
- *    *                 the alphabet a - z
- *   *
- *   */
+ *   *                 the alphabet a - z
+ *  
+ */
 
 void print_alphabet(void)
 {
