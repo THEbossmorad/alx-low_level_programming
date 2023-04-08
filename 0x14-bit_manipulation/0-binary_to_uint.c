0b49dd592aa0c27fce0a7200e455636b05b7b1ec
THEbossmorad <morad.abbous@gmail.com>
2023-04-08 19:22:36 +0000

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to int
+ * @b: pointer to str of 0 and 1 chars
+ * Return: converted number or NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int bango = 0;
+	int hajimeru = 0, l = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[l])
+		l++;
+
+	while (l--)
+	{
+		if (b[l] == '1')
+			bango += (1 << hajimeru);
+		else if (b[l] != '0')
+			return (0);
+		hajimeru++;
+	}
+
+	return (bango);
+}
